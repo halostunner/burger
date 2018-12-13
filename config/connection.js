e30310c13acfd4b2e7b31c5d34fdff32f916fa13
parminderothi@msn.com <41976903+halostunner@users.noreply.github.com>
2018-12-13 16:28:10 -0500

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,23 @@
 let mysql = require("mysql");
 
-let connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL); 
+
+}  else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: "root",
   database: "burgers_db"
-});
+
+  });
+
+  
+
+}
+
+
 
 connection.connect(function(err) {
   if (err) {
@@ -19,13 +30,4 @@ connection.connect(function(err) {
 module.exports = connection;
 
 
-
-
-
-
-
-
-
-
-
-
+  
