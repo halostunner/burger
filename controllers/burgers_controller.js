87447b566772764a29b76da53ac6a9d73f3da35b
parminderothi@msn.com <41976903+halostunner@users.noreply.github.com>
2018-12-13 19:28:43 -0500

    new

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,7 @@ let burger = require("../models/burger.js");
 
 
 router.get("/", function(req, res) {
-  burger.selectAll(function(data) {
+  burger.all(function(data) {
     let hbsObject = {
       burgers: data
     };
@@ -16,7 +16,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.insertOne([
+  burger.insert([
     "burger_name", "devoured"
   ], [
     req.body.burger_name, req.body.devoured
@@ -31,7 +31,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  burger.updateOne({
+  burger.update({
     devoured: req.body.sleepy
   }, condition, function(result) {
     if (result.changedRows == 0) {
